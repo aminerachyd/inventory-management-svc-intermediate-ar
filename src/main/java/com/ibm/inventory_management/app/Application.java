8387abccdc155cc175038afafabef32b3e60be17
Amine RACHYD <aminerachyd99@gmail.com>
2022-07-13 15:12:00 +0200

    Adds StockItemController

--- src/main/java/com/ibm/inventory_management/app/Application.java
+++ src/main/java/com/ibm/inventory_management/app/Application.java
@@ -11,29 +11,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-@ComponentScan({"com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@EnableSwagger2
+@ComponentScan({ "com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health" })
 public class Application extends SpringBootServletInitializer {
-      @Autowired
-          Environment environment;
+    @Autowired
+    Environment environment;
 
-          public static void main(String[] args) {
-                    SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
-                        }
+    public static void main(String[] args) {
+        SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
+    }
 
-              @Bean
-                  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-                            return args -> {
-                                          String port = environment.getProperty("local.server.port");
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            String port = environment.getProperty("local.server.port");
 
-                                                      System.out.println();
-                                                                  System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
-                                                                          };
-                                }
+            System.out.println();
+            System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
+        };
+    }
 
-                  @Override
-                      protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-                                return application.sources(Application.class);
-                                    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
-
